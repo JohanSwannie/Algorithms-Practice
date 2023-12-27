3956c94c915f8f02190efe6c9fa48e9f32a51632
Johan Swan <jswannieman@gmail.com>
2023-12-28 10:17:54 +1300

    Commit algorithms-practice Javascript Component

--- algorithms-practice.js
+++ algorithms-practice.js
@@ -7,7 +7,6 @@ const generatePtag = (innerContent) => {
 };
 
 //********************************************************************************* //
-//* NUMBER 1
 //* Sum all the numbers in a range with 2 numbers given in or out of sequence
 //* Long way
 //********************************************************************************* //
@@ -23,5 +22,25 @@ const sumLongWay = (arr1) => {
   return total1;
 };
 
-generatePtag(`Sum the numbers between 8 and 17 the long way &#8594;
-${sumLongWay([17, 8])}`);
+generatePtag(
+  `Sum the numbers between 8 and 17 the long way &#8594; ${sumLongWay([17, 8])}`
+);
+
+//********************************************************************************* //
+//* Sum all the numbers in a range with 2 numbers given in or out of sequence
+//* Short way
+//********************************************************************************* //
+
+const sumShortWay = (arr2) => {
+  let total2 = 0;
+  for (let i = Math.min(...arr2); i <= Math.max(...arr2); i++) {
+    total2 += i;
+  }
+  return total2;
+};
+
+generatePtag(
+  `Sum the numbers between 3 and 21 the short way &#8594; ${sumShortWay([
+    3, 21,
+  ])}`
+);
